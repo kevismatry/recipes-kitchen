2448cc705ebc82f3143ec4e893fec5e9c602fa58
kevismatry <ismaelmaxi99@gmail.com>
2024-07-04 21:34:47 -0300

    feat: cambie el camelcase

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const express = require('express');
      const recipes = [
       {
         id: 1,
-        RecipeName: "Arroz con pollo",
+        recipeName: "Arroz con pollo",
         servings: 4,
-        PreparationTime: 45,
-        StepbyStep: [
+        preparationTime: 45,
+        stepbyStep: [
           "1. En una olla, calentar aceite y freír el pollo hasta dorar.",
           "2. Agregar el arroz y mezclar bien.",
           "3. Añadir el caldo y los condimentos.",
           "4. Cocinar a fuego medio hasta que el arroz esté tierno.",
           "5. Servir caliente."
         ],
-        "ImageName": "receta_1_arroz_con_pollo.jpg"
+        imageName: "https://imgur.com/a/RxKHRj3"
       },
       {
         id: 2,
-        RecipeName: "Spaghetti a la bolognesa",
+        recipeName: "Spaghetti a la bolognesa",
         servings: 6,
-        PreparationTime: 60,
-        StepbyStep: [
+        preparationTime: 60,
+        stepbyStep: [
           "1. En una sartén, calentar aceite y sofreír la cebolla y el ajo.",
           "2. Agregar la carne molida y cocinar hasta que esté dorada.",
           "3. Añadir la salsa de tomate y los condimentos.",
@@ -37,10 +37,10 @@ const express = require('express');
       },
       {
         id: 3,
-        RecipeName: "Ensalada César",
+        recipeName: "Ensalada César",
         servings: 2,
-        PreparationTime: 15,
-        StepbyStep: [
+        preparationTime: 15,
+        stepbyStep: [
           "1. Lavar y picar la lechuga en trozos pequeños.",
           "2. En un tazón grande, mezclar la lechuga con el aderezo César.",
           "3. Agregar los croutones y el queso parmesano.",
@@ -51,10 +51,10 @@ const express = require('express');
       },
       {
         id: 4,
-        RecipeName: "Tacos de carne asada",
+        recipeName: "Tacos de carne asada",
         servings: 8,
-        PreparationTime: 30,
-        StepbyStep: [
+        preparationTime: 30,
+        stepbyStep: [
           "1. Cortar la carne en tiras delgadas.",
           "2. En una sartén, calentar aceite y freír la carne hasta que esté dorada.",
           "3. Calentar las tortillas en un comal o sartén.",
@@ -65,10 +65,10 @@ const express = require('express');
       },
       {
         id: 5,
-        RecipeName: "Hamburguesa casera",
+        recipeName: "Hamburguesa casera",
         servings: 4,
-        PreparationTime: 30,
-        StepbyStep: [
+        preparationTime: 30,
+        stepbyStep: [
           "1. En un tazón grande, mezclar la carne molida con los condimentos.",
           "2. Formar las hamburguesas y refrigerar durante 1 hora.",
           "3. En una sartén o parrilla, cocinar las hamburguesas a fuego medio-alto durante 4-5 minutos por cada lado.",
