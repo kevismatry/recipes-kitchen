08bf8498038fd3af91cfaf63484888c90a779aa0
kevismatry <ismaelmaxi99@gmail.com>
2024-07-19 23:20:25 -0300

    importe corps y lo añadi en mi lista de recetas

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const cors = require('cors')
 const express = require('express');
  const app = express();
  const port = 3000;
-
+  app.use(cors());
  app.get('/', (req, res) => res.send('Hello World!'));
 
  
